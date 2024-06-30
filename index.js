8f98440213af4062ad2a4c8383348d3c1a87f796
Houssem Khalfaoui <gameplayclip4@gmail.com>
2024-06-30 20:17:29 +0100

    exercice 2

---
+++ index.js
@@ -0,0 +1,20 @@
+const dataArray = require('./sData.js');
+
+function countSurveysPerDay(dataArray) {
+  const surveyCounts = {};
+
+  dataArray.forEach(survey => {
+    const date = new Date(survey.createdDate * 1000).toISOString().split('T')[0];
+
+    if (!surveyCounts[date]) {
+      surveyCounts[date] = 1;
+    } else {
+      surveyCounts[date]++;
+    }
+  });
+
+  return surveyCounts;
+}
+
+const surveyCountsPerDay = countSurveysPerDay(dataArray);
+console.log(surveyCountsPerDay);
